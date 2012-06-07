7170ed118a42a8e77d7658da544102a1bb337851
tedonema <marco.tedone@gmail.com>
2012-06-07 21:33:55 +0100

    Completed exercise 1-19

---
+++ src/reverse-string-1-19.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+
+#define MAXLINE  			1000
+#define IS_BLANK_LINE		1
+#define IS_NOT_BLANK_LINE	0
+
+/** Fills the sink with a line and returns the non-blank characters read */
+int getLine(char sink[]);
+
+void reverse(int length, char source[], char target[]);
+
+
+int main() {
+
+	int read;
+
+	char source[MAXLINE];
+
+	char target[MAXLINE];
+
+	while ((read = getLine(source)) >= 0) {
+
+		if (read > 0)
+		{
+
+			//Got a line. Now reverse it
+			reverse(read - 1, source, target);
+			printf("Reversed line: %s\n", target);
+
+		}
+	}
+
+	return 0;
+}
+
+int getLine(char sink[]) {
+
+	int i, c, inWord;
+
+	inWord = 0;
+
+	for (i = 0; i < MAXLINE - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
+		sink[i] = c;
+
+	//It closes the array with the null character
+	sink[i] = '\0';
+
+	if (c == EOF) i = -1;
+
+	return i;
+
+}
+
+/*
+ * length: length of source
+ * source: the line to be reversed
+ * target: the sink where the line gets reversed
+ */
+void reverse(int length, char source[], char target[])
+{
+
+
+	int i, j;
+
+	i = 0;
+
+	for (j = length; j >= 0; --j)
+	{
+		target[i] = source[j];
+
+		i++;
+	}
+
+	target[++length] = '\0';
+
+}
+
+
